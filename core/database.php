0749de74de1f0ac80b3caa04025625ecc45c1b69
Clive Verghese <cliveverghese@gmail.com>
2011-08-01 21:18:31 +0530

    folder structure created

---
+++ core/database.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+This file creates the connect to the database with the configurations
+stored in the config.php file. If the database does not exist then the
+new database is created.
+*/	
+
+	require_once("config.php");
+
+
+?>
