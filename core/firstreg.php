a4e8ac8d64a416cd43554623b48cfd18c1ebba42
Clive Verghese <cliveverghese@gmail.com>
2011-08-06 20:52:08 +0530

    Test message removed during admin reg

--- core/firstreg.php
+++ core/firstreg.php
@@ -5,7 +5,6 @@ function admin_reg()
 	$user = $_GET["user"];
 	$pass = sha1($_GET["password"]);
 	$sql = "INSERT INTO user(username,password) VALUES ('" . mysql_real_escape_string($user) . "','" . mysql_real_escape_string($pass) . "')";
-	echo $sql;
 	query_database($sql);
 }
 ?>
